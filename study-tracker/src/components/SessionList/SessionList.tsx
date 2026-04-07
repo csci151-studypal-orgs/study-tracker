c293b960d8c087674c6d3ac5c2c4ba1ea115b5a5
Allyson Jhen S Piedraverde <22-1-02605@vsu.edu.ph>
2026-04-07 11:54:24 +0800

    feat: Add delete confirmation modal

--- study-tracker/src/components/SessionList/SessionList.tsx
+++ study-tracker/src/components/SessionList/SessionList.tsx
@@ -1,7 +1,6 @@
 import { useState, useMemo } from 'react';
 import type { StudySession } from '../../types';
 import SessionItem from './SessionItem';
-import DeleteConfirm from './DeleteConfirm';
 import SessionForm from '../SessionForm/SessionForm';
 
 interface SessionListProps {
@@ -38,13 +37,6 @@ export default function SessionList({ sessions, onUpdate, onDelete }: SessionLis
   const handleEditSave = (updated: StudySession) => {
     onUpdate(updated.id, updated);
     setEditingSession(null);
-  };
-
-  const handleDeleteConfirm = () => {
-    if (deletingSession) {
-      onDelete(deletingSession.id);
-      setDeletingSession(null);
-    }
   };
 
   return (
@@ -104,14 +96,6 @@ export default function SessionList({ sessions, onUpdate, onDelete }: SessionLis
         />
       )}
 
-      {/* Delete confirm */}
-      {deletingSession && (
-        <DeleteConfirm
-          session={deletingSession}
-          onConfirm={handleDeleteConfirm}
-          onCancel={() => setDeletingSession(null)}
-        />
-      )}
     </div>
   );
 }
